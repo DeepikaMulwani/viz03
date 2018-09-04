a34cece2558660369a97aa0ff23670d1a5975397
DeepikaMulwani <deepika.mulwani@gmail.com>
2018-09-03 22:50:30 -0700

    Initial Commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var viz03A6720BF99D30432F84C25B6FE716F89D_DEBUG = {
+        name: 'viz03A6720BF99D30432F84C25B6FE716F89D_DEBUG',
+        displayName: 'viz03',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.1.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.viz03A6720BF99D30432F84C25B6FE716F89D.Visual(options),
+        custom: true
+    };
+}
